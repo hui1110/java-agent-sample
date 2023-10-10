7eec09cec2fc13e16529e99a4df87c1df8a13b87
hui1110 <hui1110rui@163.com>
2023-10-10 15:09:14 +0800

    add sample code

---
+++ src/main/java/com/example/producer/KafkaProducer.java
@@ -0,0 +1,24 @@
+package com.example.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(String str) {
+        kafkaTemplate.send("testTopic", str);
+        logger.info("Send message: " + str);
+    }
+
+}
