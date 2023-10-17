a49cf85a4bd3d493121e28b685ef97acfc63565e
hui1110 <hui1110rui@163.com>
2023-10-17 15:17:48 +0800

    update file

--- src/main/java/com/example/consumer/KafkaConsumerListener.java
+++ src/main/java/com/example/consumer/KafkaConsumerListener.java
@@ -1,18 +1,18 @@
-package com.example.consumer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class KafkaConsumerListener {
-
-    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerListener.class);
-
-    @KafkaListener(topics = "testTopic", groupId = "testGroup")
-    public void onMessage(String str){
-        logger.info("Receive message: " + str);
-    }
-
-}
+//package com.example.consumer;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class KafkaConsumerListener {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerListener.class);
+//
+//    @KafkaListener(topics = "testTopic", groupId = "testGroup")
+//    public void onMessage(String str){
+//        logger.info("Receive message: " + str);
+//    }
+//
+//}
